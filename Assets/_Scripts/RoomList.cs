9e0eaf64a613d5f66fdabd418f7f9c940ec9f1af
lieuquichuong2005 <lieuquichuong2005@gmail.com>
2024-11-14 00:11:01 +0700

    làm scene lobby

--- Assets/_Scripts/RoomList.cs
+++ Assets/_Scripts/RoomList.cs
@@ -5,13 +5,26 @@ using Photon.Realtime;
 public class RoomList : MonoBehaviourPunCallbacks
 {
     public GameObject roomPrefab;
+    public GameObject[] allRooms;
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
+        for(int i = 0; i < allRooms.Length; i++)
+        {
+            if (allRooms[i] == null)
+                Destroy(allRooms[i]);
+        }
+        allRooms = new GameObject[roomList.Count];
+
         for(int i = 0; i < roomList.Count; i++)
         {
-            GameObject room = Instantiate(roomPrefab, Vector3.zero, Quaternion.identity, GameObject.Find("Content").transform);
-            room.GetComponent<Room>().nameRoom.text = roomList[i].Name;
+            if (roomList[i].IsOpen && roomList[i].IsVisible && roomList[i].PlayerCount >= 1)
+            {
+                GameObject room = Instantiate(roomPrefab, Vector3.zero, Quaternion.identity, GameObject.Find("Content").transform);
+                room.GetComponent<Room>().nameRoom.text = roomList[i].Name;
+
+                allRooms[i] = room;
+            }
         }
     }
 
