5c30e7d5493b56a408d13ab0a1ae2ba4609753dc
Lam Tri Duc <63941511+krislam2001@users.noreply.github.com>
2024-11-18 01:23:59 +0700

    Update

---
+++ Assets/_Scripts/PlayerInteract.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public HealthBar healthBar;
+    int current_health;
+
+    void Start()
+    {
+        current_health = 100;
+        healthBar.SetMaxHealth(100);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+
+            int damage = collision.gameObject.GetComponent<MonsterInteract>().GetDamage();
+
+            current_health -= damage;
+
+            healthBar.SetHealth(current_health);
+        }
+    }
+}
--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -5,29 +5,25 @@ using Photon.Pun;
 
 public class PlayerMovement : MonoBehaviourPun
 {
-    PlayerStats playerStats;
-
-    private SpriteRenderer sprite;
-    public Sprite[] sprites;
+    //public static PlayerMovement instance;
 
 
     private Rigidbody2D rb2d;
     public Animator animator;
 
-    public bool isCanMove = true;
+    //public bool isCanMove = true;
     bool isMove;
+
+    public float moveSpeed;
 
     //public float weaponSpeed;
     private float moveX;
     private float moveY;
 
-
     //float timeUntilWeapon;
-
     //public GameObject WeaponRotate;
 
     //public List<PlayerItems> items = new List<PlayerItems>();
-
 
     public Transform attackPoint;
     public float attackRange = 0.5f;
@@ -35,9 +31,13 @@ public class PlayerMovement : MonoBehaviourPun
 
     public int damage = 20;
 
+    void Awake()
+    {
+
+    }
+
     void Start()
     {
-        playerStats = GetComponent<PlayerStats>();
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         DontDestroyOnLoad(this.gameObject);
@@ -45,97 +45,138 @@ public class PlayerMovement : MonoBehaviourPun
 
         isMove = false;
 
-        sprite = GetComponent<SpriteRenderer>();
-
     }
 
     void Update()
     {
-        if (photonView.IsMine && isCanMove)
-        {
+
+            moveSpeed = (Input.GetKey(KeyCode.LeftShift)) ? 8f : 4f;
             moveX = Input.GetAxis("Horizontal");
             moveY = Input.GetAxis("Vertical");
 
-            //Debug.Log("moveX: " + moveX + "moveY: " + moveY);
-
-            // Di chuyển và cập nhật hoạt ảnh
-            if (moveX != 0 || moveY != 0)
-            {
-                animator.speed = 1f; // Bật hoạt ảnh khi di chuyển
-                rb2d.linearVelocity = new Vector2(moveX * playerStats.moveSpeed, moveY * playerStats.moveSpeed);
-                animator.SetFloat("MoveX", moveX);
-                animator.SetFloat("MoveY", moveY);
-
-
-                UpdateSprite(); // Cập nhật sprite theo hướng
-            }
-            else
-
-            {
-                animator.speed = 0f;
-                rb2d.linearVelocity = Vector2.zero; // Dừng di chuyển khi không có input   
-
-            }
-            if (Input.GetKey(KeyCode.Space))
-            {
-                animator.SetBool("isAttack", true);
-                /*Collider2D[] hitenemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-
-                foreach(Collider2D enemy in hitenemies)
-                {
-                    if (enemy.tag == "enemy")
-                    {
-                        Debug.Log("Hello");
-                        enemy.gameObject.GetComponent<MonsterInteract>().ReceiveDamage(damage);
-                        break;
-                    }
-                }*/
-            }
-
+        if (moveX != 0 || moveY != 0)
+        {
+            isMove = true;
+            rb2d.linearVelocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
+            animator.SetFloat("moveX", moveX);
+            animator.SetFloat("moveY", moveY);
         }
+        else
+        {
+            isMove = false;
+            rb2d.linearVelocity = Vector2.zero;
+        }
+
+        animator.SetBool("isMove", isMove);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("isAttack", true);
+            /*Collider2D[] hitenemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+            
+            foreach(Collider2D enemy in hitenemies)
+            {
+                if (enemy.tag == "enemy")
+                {
+                    Debug.Log("Hello");
+                    enemy.gameObject.GetComponent<MonsterInteract>().ReceiveDamage(damage);
+                    break;
+                }
+            }*/
+        }
+
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Attack");
+        }*/
     }
 
-    private void UpdateSprite()
+    /*void Update()
+    {
+        //if (photonView.IsMine)
+        //{
+            if (isCanMove)
+            {
+                moveSpeed = (Input.GetKey(KeyCode.LeftShift)) ? 8f : 4f;
+                moveX = Input.GetAxis("Horizontal");
+                moveY = Input.GetAxis("Vertical");
+                //Debug.Log("moveX: " + moveX + "moveY: " + moveY);
+                // Di chuyển và cập nhật hoạt ảnh
+                //if (moveX != 0 || moveY != 0)
+                //{
+                    rb2d.linearVelocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
+                    animator.SetFloat("MoveX", moveX);
+                    animator.SetFloat("MoveY", moveY);
+                    animator.speed = 1f; // Bật hoạt ảnh khi di chuyển
+
+                    //UpdateSprite(); // Cập nhật sprite theo hướng
+                //}
+                *//*else
+                {
+                    rb2d.linearVelocity = Vector2.zero; // Dừng di chuyển khi không có input   
+                }*//*
+            }
+        //}
+
+        if (timeUntilWeapon <= 0f)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                animator.SetTrigger("Attack");
+                timeUntilWeapon = weaponSpeed;
+            }
+        }
+        else
+        {
+            timeUntilWeapon -= Time.deltaTime;
+        }
+    }*/
+
+
+    /*private void UpdateSprite()
     {
         var rotationVector = transform.rotation.eulerAngles;
 
         if (moveY > 0) // Hướng lên
         {
+
+            animator.SetFloat("MoveY", 0);
             rotationVector.z = 0f;
-            sprite.sprite = sprites[0];
         }
         else if (moveY < 0) // Hướng xuống
         {
+            animator.SetFloat("MoveY", 0);
             rotationVector.z = 180f;
-            sprite.sprite = sprites[1];
         }
         else if (moveX < 0) // Hướng trái
         {
+            animator.SetFloat("MoveX", 0);
             rotationVector.z = 90f;
-            sprite.sprite = sprites[2];
         }
         else if (moveX > 0) // Hướng phải
         {
+            animator.SetFloat("MoveX", 0);
             rotationVector.z = -90f;
-            sprite.sprite = sprites[3];
         }
-    }
 
+        WeaponRotate.transform.rotation = Quaternion.Euler(rotationVector);
+    }*/
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "enemy")
         {
             Debug.Log("Hit");
+
         }
     }
 
-    public void EndAttack()
+    public void endAttack()
     {
         animator.SetBool("isAttack", false);
     }
 
-    public void Attack()
+    public void attack()
     {
         Collider2D[] hitenemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
