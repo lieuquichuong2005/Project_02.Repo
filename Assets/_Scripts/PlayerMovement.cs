f2ca0ce56ed8dbf354300d4b3ca69300f7b9c765
lieuquichuong2005 <lieuquichuong2005@gmail.com>
2024-11-17 23:59:14 +0700

    Làm Scene Rừng Xanh

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -5,8 +5,7 @@ using Photon.Pun;
 
 public class PlayerMovement : MonoBehaviourPun
 {
-    //public static PlayerMovement instance;
-
+    PlayerStats playerStats;
 
     private SpriteRenderer sprite;
     public Sprite[] sprites;
@@ -15,10 +14,9 @@ public class PlayerMovement : MonoBehaviourPun
     private Rigidbody2D rb2d;
     public Animator animator;
 
-    //public bool isCanMove = true;
+    public bool isCanMove = true;
     bool isMove;
 
-    public float moveSpeed;
     //public float weaponSpeed;
     private float moveX;
     private float moveY;
@@ -37,14 +35,9 @@ public class PlayerMovement : MonoBehaviourPun
 
     public int damage = 20;
 
-    void Awake()
-    {
-
-    }
-
-
     void Start()
     {
+        playerStats = GetComponent<PlayerStats>();
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         DontDestroyOnLoad(this.gameObject);
@@ -58,114 +51,51 @@ public class PlayerMovement : MonoBehaviourPun
 
     void Update()
     {
-
-            moveSpeed = (Input.GetKey(KeyCode.LeftShift)) ? 8f : 4f;
+        if (photonView.IsMine && isCanMove)
+        {
             moveX = Input.GetAxis("Horizontal");
             moveY = Input.GetAxis("Vertical");
 
-        if (moveX != 0 || moveY != 0)
-        {
-            isMove = true;
-            rb2d.linearVelocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
-            animator.SetFloat("moveX", moveX);
-            animator.SetFloat("moveY", moveY);
-        }
-        else
-        {
-            isMove = false;
-            rb2d.linearVelocity = Vector2.zero;
-        }
+            //Debug.Log("moveX: " + moveX + "moveY: " + moveY);
 
-        animator.SetBool("isMove", isMove);
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            animator.SetBool("isAttack", true);
-            /*Collider2D[] hitenemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-            
-            foreach(Collider2D enemy in hitenemies)
+            // Di chuyển và cập nhật hoạt ảnh
+            if (moveX != 0 || moveY != 0)
             {
-                if (enemy.tag == "enemy")
-                {
-                    Debug.Log("Hello");
-                    enemy.gameObject.GetComponent<MonsterInteract>().ReceiveDamage(damage);
-                    break;
-                }
-            }*/
-        }
+                animator.speed = 1f; // Bật hoạt ảnh khi di chuyển
+                rb2d.linearVelocity = new Vector2(moveX * playerStats.moveSpeed, moveY * playerStats.moveSpeed);
+                animator.SetFloat("MoveX", moveX);
+                animator.SetFloat("MoveY", moveY);
 
-        /*if (Input.GetMouseButtonDown(0))
-        {
-            animator.SetTrigger("Attack");
-        }*/
-    }
 
-    /*void Update()
-    {
-        //if (photonView.IsMine)
-        //{
+                UpdateSprite(); // Cập nhật sprite theo hướng
+            }
+            else
 
-            if (isCanMove)
             {
-                moveSpeed = (Input.GetKey(KeyCode.LeftShift)) ? 8f : 4f;
-                moveX = Input.GetAxis("Horizontal");
-                moveY = Input.GetAxis("Vertical");
-
-                //Debug.Log("moveX: " + moveX + "moveY: " + moveY);
-
-                // Di chuyển và cập nhật hoạt ảnh
-                if (moveX != 0 || moveY != 0)
-                {
-                    animator.speed = 1f; // Bật hoạt ảnh khi di chuyển
-                    rb2d.linearVelocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
-                    animator.SetFloat("MoveX", moveX);
-                    animator.SetFloat("MoveY", moveY);
-
-
-                    //UpdateSprite(); // Cập nhật sprite theo hướng
-                //}
-                *//*else
-
-                {
-                    animator.speed = 0f;
-                    rb2d.linearVelocity = Vector2.zero; // Dừng di chuyển khi không có input   
-
-                }*//*
-
-                }
-                if (timeUntilWeapon <= 0f)
-                {
-                    if (Input.GetKey(KeyCode.Space))
-                    {
-                        animator.SetTrigger("Attack");
-                        timeUntilWeapon = weaponSpeed;
-                    }
-                }
-                else
-                {
-                    timeUntilWeapon -= Time.deltaTime;
-                }
+                animator.speed = 0f;
+                rb2d.linearVelocity = Vector2.zero; // Dừng di chuyển khi không có input   
 
             }
-        }
-
-
-        if (timeUntilWeapon <= 0f)
-        {
             if (Input.GetKey(KeyCode.Space))
             {
-                animator.SetTrigger("Attack");
-                timeUntilWeapon = weaponSpeed;
+                animator.SetBool("isAttack", true);
+                /*Collider2D[] hitenemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+                foreach(Collider2D enemy in hitenemies)
+                {
+                    if (enemy.tag == "enemy")
+                    {
+                        Debug.Log("Hello");
+                        enemy.gameObject.GetComponent<MonsterInteract>().ReceiveDamage(damage);
+                        break;
+                    }
+                }*/
             }
-        }
-        else
-        {
-            timeUntilWeapon -= Time.deltaTime;
-        }
-    }*/
 
+        }
+    }
 
-    /*private void UpdateSprite()
+    private void UpdateSprite()
     {
         var rotationVector = transform.rotation.eulerAngles;
 
@@ -189,9 +119,7 @@ public class PlayerMovement : MonoBehaviourPun
             rotationVector.z = -90f;
             sprite.sprite = sprites[3];
         }
-
-        WeaponRotate.transform.rotation = Quaternion.Euler(rotationVector);
-    }*/
+    }
 
 
     void OnTriggerEnter2D(Collider2D collider)
@@ -202,12 +130,12 @@ public class PlayerMovement : MonoBehaviourPun
         }
     }
 
-    public void endAttack()
+    public void EndAttack()
     {
         animator.SetBool("isAttack", false);
     }
 
-    public void attack()
+    public void Attack()
     {
         Collider2D[] hitenemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
--- Assets/_Scripts/PlayerStats.cs
+++ Assets/_Scripts/PlayerStats.cs
@@ -1,16 +1,35 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Slider healthSlider;
+    public Slider manaSlider;
 
-    // Update is called once per frame
-    void Update()
+    public TMP_Text levelText;
+
+    private float currentHealth;
+    private float currentMana;
+    private float maxHealth = 100;
+    private float maxMana = 100;
+    
+    public float moveSpeed;
+    private float level = 1;
+
+    private void Awake()
     {
-        
+        healthSlider = GameObject.FindWithTag("HealthSlider").GetComponent<Slider>();
+        manaSlider = GameObject.FindWithTag("ManaSlider").GetComponent <Slider>();
+        levelText = GameObject.FindWithTag("LevelText").GetComponent <TMP_Text>();
+
+        currentHealth = maxHealth;
+        currentMana = maxMana;
+        moveSpeed = 3f;
+
+        healthSlider.value = currentHealth / maxHealth;
+        manaSlider.value = currentMana / maxMana;
+        levelText.text = level.ToString();
+
     }
 }
