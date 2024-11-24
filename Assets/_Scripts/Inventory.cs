41e1478438a7a9087fd58db2babe6b30502ed20a
lieuquichuong2005 <lieuquichuong2005@gmail.com>
2024-11-24 22:02:09 +0700

    Làm inventory

---
+++ Assets/_Scripts/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+public class Inventory
+{
+    private List<Item> items = new List<Item>();
+    public int maxItems = 20; // Giới hạn số vật phẩm có thể lưu
+
+    public bool AddItem(Item item)
+    {
+        if (items.Count < maxItems)
+        {
+            items.Add(item);
+            items.Add(item);
+            return true; // Thêm thành công
+        }
+        return false; // Hết chỗ
+    }
+
+    public bool RemoveItem(Item item)
+    {
+        return items.Remove(item);
+    }
+
+    public List<Item> GetItems()
+    {
+        return items;
+    }
+}
--- Assets/_Scripts/PlayerCollider.cs
+++ Assets/_Scripts/PlayerCollider.cs
@@ -1,21 +1,28 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerCollider : MonoBehaviour
 {
     PlayerStats playerStats;
+    InventoryUI inventoryUI;
 
     private void Start()
     {
         playerStats = GetComponent<PlayerStats>();
+        inventoryUI = GetComponent<InventoryUI>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if(collision.gameObject.CompareTag("Item"))
+        if(other.gameObject.CompareTag("Item"))
         {
-            Destroy(collision.gameObject);
+            if (other.CompareTag("Item"))
+            {
+                //Item item = other.GetComponent<ItemPickup>().item; // Giả sử bạn có một script ItemPickup
+                //inventoryUI.AddItemToInventory(item);
+                Destroy(other.gameObject); // Xóa vật phẩm khỏi game
+            }
         }
-        if(collision.gameObject.CompareTag("Monster"))
+        if(other.gameObject.CompareTag("Monster"))
         {
             playerStats.currentHealth -= 2;
         }
